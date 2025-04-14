c36a9483cc810786004ea9c273259ba86162b7f8
Wittner Attila <wiattila8@gmail.com>
2025-04-14 18:44:52 +0200

    it works :3

--- Timetable/app.js
+++ Timetable/app.js
@@ -1,59 +1,90 @@
 import express from 'express';
+import cors from "cors";
 import { dbAll, dbGet, dbRun, initDb } from './util/database.js';
 
 const port = 3000;
 const app = express();
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
 app.get("/table", async (req, res) => {
-    const timetable = await dbAll("SELECT * FROM Timetable");
+    const monday = await dbAll("SELECT * FROM Monday");
+    const tuesday = await dbAll("SELECT * FROM Tuesday");
+    const wednesday = await dbAll("SELECT * FROM Wednesday");
+    const thursday = await dbAll("SELECT * FROM Thursday");
+    const friday = await dbAll("SELECT * FROM Friday");
+    const timetable = {
+        Monday: monday,
+        Tuesday: tuesday,
+        Wednesday: wednesday,
+        Thursday: thursday,
+        Friday: friday
+    }
     res.status(200).json(timetable)
 })
 
-app.get("/table/:id", async (req, res) => {
-    const id = req.params.id;
-    const user = await dbGet("SELECT * FROM Timetable WHERE id=?", [id])
-    if (!user){
-        return res.status(404).json({message:"User not found"})
-    }
-    res.status(200).json(user);
-})
-
-app.post("/Timetable", async (req, res) => {
-    const {dayName, classes} = req.body;
-    if (!dayName || !classes) {
-        return res.status(400).json({message: `Fill all fields`})
-    }
-    const result = await dbRun(`INSERT INTO Timetable (name, age) VALUES (?, ?);`, [name, age]);
-    res.status(201).json({id: result.lastID, dayName, classes});
-})
-
-app.put("/Timetable/:id", async (req, res) => {
-    const id = req.params.id;
-    const day = await dbGet("SELECT * FROM Timetable WHERE id=?", [id])
+app.get("/:day", async (req, res) => {
+    const dayName = req.params.day;
+    dayName[0].toUpperCase();
+    const day = await dbAll(`SELECT * FROM ${dayName}`)
     if (!day){
         return res.status(404).json({message:"Day not found"})
     }
-    const {dayName, classes} = req.body;
-    if (!dayName || !classes) {
-        return res.status(400).json({message: `Fill all fields`})
-    }
-    await dbRun("UPDATE Timetable SET day = ?, class0 = ?, class1 = ?, class2 = ?, class3 = ?, class4 = ?, class5 = ?, class6 = ?, class7 = ?, class8 = ?, class9 = ?, class10 = ? WHERE id = ?;", [dayName, classes[0], classes[1], classes[2], classes[3], classes[4], classes[5], classes[6], classes[7], classes[8], classes[9], classes[10], id])
-    res.status(200).json({id, dayName, classes});
+    res.status(200).json(day);
 })
 
-app.delete("/Timetable/:id", async (req, res) => {
+app.get("/:day/:id", async (req, res) => {
     const id = req.params.id;
-    const day = await dbGet("SELECT * FROM Timetable WHERE id=?", [id])
+    const dayName = req.params.day;
+    dayName[0].toUpperCase();
+    const day = await dbGet(`SELECT * FROM ${dayName} WHERE id=?`, [id])
     if (!day){
         return res.status(404).json({message:"Day not found"})
     }
-    dbRun("DELETE FROM Timetable WHERE id = ?", [id])
-    res.status(201).json({message: "Deleted day"})
+    res.status(200).json(day);
+})
+
+app.post("/:day", async (req, res) => {
+    const dayName = req.params.day;
+    dayName[0].toUpperCase();
+    const {className, teacher, room, period} = req.body;
+    if (!className || !teacher || !room || !period) {
+        return res.status(400).json({message: `Fill all fields`})
+    }
+    const result = await dbRun(`INSERT INTO ${dayName} (className, teacher, room, period) VALUES (?, ?, ?, ?);`, [className, teacher, room, period]);
+    res.status(201).json({id: result.lastID, className, teacher, room, period});
+})
+
+app.put("/:day/:id", async (req, res) => {
+    const id = req.params.id;
+    const dayName = req.params.day;
+    dayName[0].toUpperCase();
+    const {className, teacher, room, period} = req.body;
+    const day = await dbGet(`SELECT * FROM ${dayName} WHERE id=?`, [id])
+    if (!day){
+        return res.status(404).json({message:"Day not found"})
+    }
+    if (!className || !teacher || !room || !period) {
+        return res.status(400).json({message: `Fill all fields`})
+    }
+    await dbRun(`UPDATE ${dayName} SET className=?, teacher=?, room=?, period=? WHERE id=?`, [className, teacher, room, period, id])
+    res.status(200).json({id, className, teacher, room, period});
+})
+
+app.delete("/:day/:id", async (req, res) => {
+    const id = req.params.id;
+    const dayName = req.params.day;
+    dayName[0].toUpperCase();
+    const day = await dbGet(`SELECT * FROM ${dayName} WHERE id=?`, [id])
+    if (!day){
+        return res.status(404).json({message:"Class not found"})
+    }
+    dbRun(`DELETE FROM ${dayName} WHERE id=?`, [id])
+    res.status(201).json({message: "Deleted class"})
 })
 
 // HAS TO BE LAST
-app.use((req, res, next, err) => {
+app.use((err, req, res, next) => {
     if (err){
         res.status(500).json({message: `Error: ${err.message}`})
     }
--- Timetable/util/database.js
+++ Timetable/util/database.js
@@ -30,31 +30,85 @@ export function dbGet(sql, params = []) {
 
 export function dbRun(sql, params) {
     return new Promise((resolve, reject) => {
-        db.run(sql, params, (err) => {
-            if (err){
+        db.run(sql, params, function (err) {
+            if (err) {
                 reject(err);
-            }
-            else{
-                resolve(this)
+            } else {
+                resolve({ lastID: this.lastID });
             }
         });
     });
 }
 
-export async function initDb() {
-    await dbRun("DROP TABLE IF EXISTS Timetable")
-    await dbRun("CREATE TABLE IF NOT EXISTS Timetable (id INTEGER PRIMARY KEY AUTOINCREMENT, day STRING, class0 STRING, class1 STRING, class2 STRING, class3 STRING, class4 STRING, class5 STRING, class6 STRING, class7 STRING, class8 STRING, class9 STRING, class10 STRING)")
 
-    const timetable = [
-        // day, classes(0th, 1st, 2nd, 3rd, 4th, 5th, 6th, 7th, 8th, 9th, 10th)
-        {day:"Monday", classes:["","PE","History","Math","Grammar","IT","","","","",""]},
-        {day:"Tuesday", classes:["","PE","History","Math","Grammar","IT","","","","",""]},
-        {day:"Wednesday", classes:["","PE","History","Math","Grammar","IT","","","","",""]},
-        {day:"Thursday", classes:["","PE","History","Math","Grammar","IT","","","","",""]},
-        {day:"Friday", classes:["","PE","History","Math","Grammar","IT","","","","",""]}
+export async function initDb() {
+    await dbRun("DROP TABLE IF EXISTS Monday")
+    await dbRun("CREATE TABLE IF NOT EXISTS Monday (id INTEGER PRIMARY KEY AUTOINCREMENT, className STRING, teacher STRING, room STRING, period INTEGER)")
+
+    const monday = [
+        {className:"Math", teacher:"RJJ", room:"6.", period:3},
+        {className:"P.English", teacher:"CsB", room:"7.", period:4},
+        {className:"Literature", teacher:"GyZs", room:"16.", period:5}
     ]
 
-    for (const timetable of timetable){
-        await dbRun("INSERT INTO Timetable (day, class0, class1, class2, class3, class4, class5, class6, class7, class8, class9, class10) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", [table.day, table.classes[0], table.classes[1], table.classes[2], table.classes[3], table.classes[4], table.classes[5], table.classes[6], table.classes[7], table.classes[8], table.classes[9], table.classes[10]])
+    await dbRun("DROP TABLE IF EXISTS Tuesday")
+    await dbRun("CREATE TABLE IF NOT EXISTS Tuesday (id INTEGER PRIMARY KEY AUTOINCREMENT, className STRING, teacher STRING, room STRING, period INTEGER)")
+    
+    const tuesday = [
+        {className:"Civics", teacher:"KG", room:"13.", period:1},
+        {className:"PE", teacher:"BHA", room:"T", period:2},
+        {className:"W(CMS+WD)", teacher:"ZsM", room:"Szt2", period:3},
+        {className:"Literature", teacher:"GyZs", room:"16.", period:4},
+        {className:"History", teacher:"KG", room:"16.", period:5},
+        {className:"English", teacher:"SzM", room:"6.", period:6},
+        {className:"IT", teacher:"KOA", room:"nylab", period:7},
+        {className:"IT", teacher:"KOA", room:"Szt1", period:8},
+        {className:"Softwaretest", teacher:"NyT", room:"14", period:9},
+        {className:"Softwaretest", teacher:"NyT", room:"14", period:10}
+    ]
+
+    await dbRun("DROP TABLE IF EXISTS Wednesday")
+    await dbRun("CREATE TABLE IF NOT EXISTS Wednesday (id INTEGER PRIMARY KEY AUTOINCREMENT, className STRING, teacher STRING, room STRING, period INTEGER)")
+
+    const wednesday = [
+        {className:"IT", teacher:"VA", room:"tani", period:0},
+        {className:"IT", teacher:"VA", room:"tani", period:1},
+        {className:"IT", teacher:"VA", room:"tani", period:2},
+        {className:"History", teacher:"KG", room:"14", period:3},
+        {className:"className", teacher:"SzM", room:"14.", period:4},
+        {className:"Literature", teacher:"GyZs", room:"35.", period:5},
+        {className:"Math", teacher:"RJJ", room:"7.", period:6},
+        {className:"Math", teacher:"RJJ", room:"7.", period:7}
+    ]
+
+    await dbRun("DROP TABLE IF EXISTS Thursday")
+    await dbRun("CREATE TABLE IF NOT EXISTS Thursday (id INTEGER PRIMARY KEY AUTOINCREMENT, className STRING, teacher STRING, room STRING, period INTEGER)")
+
+    const thursday = [
+        {className:"IT", teacher:"VA", room:"tani", period:1},
+        {className:"IT", teacher:"VA", room:"tani", period:2},
+        {className:"PE", teacher:"BHA", room:"T", period:3},
+        {className:"PE", teacher:"BHA", room:"T", period:4},
+        {className:"History", teacher:"KG", room:"15.", period:5},
+        {className:"Math", teacher:"RJJ", room:"14.", period:6},
+        {className:"P.English", teacher:"CsB", room:"8.", period:7}
+    ]
+
+    await dbRun("DROP TABLE IF EXISTS Friday")
+    await dbRun("CREATE TABLE IF NOT EXISTS Friday (id INTEGER PRIMARY KEY AUTOINCREMENT, className STRING, teacher STRING, room STRING, period INTEGER)")
+
+    const friday = [
+        {className:"Grammar", teacher:"GyZs", room:"15.", period:1},
+        {className:"English", teacher:"SzM", room:"8.", period:2},
+        {className:"English", teacher:"SzM", room:"8.", period:3},
+        {className:"P.English", teacher:"CsB", room:"8.", period:4},
+        {className:"IT", teacher:"BR", room:"nylab", period:5},
+        {className:"History", teacher:"KG", room:"16.", period:6}
+    ]
+
+    for (const day of [{name:"Monday", classNamees:monday}, {name:"Tuesday", classNamees:tuesday}, {name:"Wednesday", classNamees:wednesday}, {name:"Thursday", classNamees:thursday}, {name:"Friday", classNamees:friday}]) {
+        for (const lesson of day.classNamees) {
+            await dbRun(`INSERT INTO ${day.name} (className, teacher, room, period) VALUES (?, ?, ?, ?)`, [lesson.className, lesson.teacher, lesson.room, lesson.period])
+        }
     }
 }
