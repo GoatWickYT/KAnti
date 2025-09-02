657497d73fa7a9a4efdc04ae39efffc74e3f0930
Wittner Attila <wiattila8@gmail.com>
2025-09-02 14:22:54 +0200

    Duális

---
+++ Arrays/Array Feladatok.js
@@ -0,0 +1,11 @@
+/*
+Hozz létre egy js fájlt, mely megoldaj a az alábbi feladatokat.
+Mindegyik feladatot függvénnyel old meg!
+
+1. getOtosLotteryNumbers - Ötöslottó számokat generál le véletlenszerűen, melyeket egy tömbben ad vissza.
+2. getSortedtLotteryNumbers - paraméterrként kapott tömböt növekvő sorrendbe rendezi, a rendezett tömböt visszaadja
+3. getNumberOfHits - két paramétert kap, egy tömböt lottószámokkal és egy tömböt a tippekkel. Visszaadja, hogy a tippekből ány egyezett meg a lottószámokkal
+4. getMonthlyLotteryArrayNumbers - négy hét lottószámait adja vissza egy tömbben, mely a heti lottószámok tömbjét tartalmazza (meghívja a getOtosLotteryNumbers függvényt)
+5. getMonthlyLotteryArrayNumbers - paraméterként kapja a négy hét lottószámainak tömbjét és visszaadja, hogy a hónapban mely számokat húzták ki. A viszatérő listában, minden szám csak egyszer szerepelhet.
+6. monthlyStatistics - paranéterként kapha a havi lottószámok tömbjét. Egy tömböt ad vissza, melynek elemei tömbök, melyben az első elem a lottószám, a második eleme, hogy a hónapban a számot hányszor húzták ki.
+*/
